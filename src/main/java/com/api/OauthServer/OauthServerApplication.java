0c8c80196f1d8a42016c8a35734f2ee9002fe950
JuditReta <edna.reta@neoris.com>
2024-06-07 19:18:01 -0600

    first commit

---
+++ src/main/java/com/api/OauthServer/OauthServerApplication.java
@@ -0,0 +1,13 @@
+package com.api.OauthServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthServerApplication.class, args);
+	}
+
+}
